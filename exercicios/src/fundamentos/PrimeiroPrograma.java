684138ea368ad7600f3c02dd3f1da164fea512aa
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2019-10-10 12:48:52 -0300

    Aula de comentarios

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,11 +1,34 @@
 package fundamentos;
 
+/**
+ * Essa classe representa ... JAVADOC
+ * 
+ * @author Gustavo Silva <@email.com>
+ * @since JDK1.0
+ * 
+ */
 public class PrimeiroPrograma {
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
+		// Uma senteça de código termina com ;
+		// Mais um comentario...
+		// Fim
 		System.out.println("Hello world!!");
+
+		/*
+		 * Linha 1 
+		 * Linha 2 
+		 * Linha 3...
+		 */
+
 		System.out.println("Hello world2!!");
 
+		
+		System.out.println("Fim"); //Aqui também funciona!
 	}
 }
