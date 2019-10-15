2401bc4be65288dedc8d55e1c094c9d3c2119d82
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2019-10-15 16:38:30 -0300

    Wrappers

---
+++ exercicios/src/fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto {
+	public static void main(String[] args) {
+		String s = "texto";
+		s.toUpperCase();
+
+		// Wrappers sao a versao objeto dos tipo primitivos!
+		int a = 123;
+		System.out.println(a);
+
+	}
+
+}
---
+++ exercicios/src/fundamentos/Wrappers.java
@@ -0,0 +1,43 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Wrappers {
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		// byte
+		Byte b = 100;
+		Short s = 1000;
+		Integer i = 10000;
+		Long l = 100000L;
+		// Integer i = Integer.parseInt(entrada.next()); //int
+
+		System.out.println(b.byteValue());
+		System.out.println(s.toString());
+		System.out.println(i * 3);
+		System.out.println(l / 3);
+		// System.out.println(i * 3);
+
+		// entrada.close();
+		
+		//Float
+		
+		Float f = 123.0F;
+		System.out.println(f);
+		
+		Double d = 1234.5678;
+		System.out.printf("%.2f\n",d);
+
+		// Boolean
+		Boolean bo = Boolean.parseBoolean("true");
+		System.out.println(bo);
+		System.out.println(bo.toString().toUpperCase());
+
+		//Caracter		
+		Character c = '#';
+		
+		System.out.println(c + "...");
+	}
+}
