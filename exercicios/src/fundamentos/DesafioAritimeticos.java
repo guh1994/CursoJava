d383349166d4a459c0fd0e7e397c177ac5c0ffe8
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2019-10-24 17:01:23 -0300

    Desafio Aritimetico

---
+++ exercicios/src/fundamentos/DesafioAritimeticos.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+public class DesafioAritimeticos {
+	public static void main(String[] args) {
+		
+		double b;
+		double c;
+		
+		double resultado;
+		
+		b = Math.pow(6*(3+2),2);
+		
+		b = b/(3*2);
+		
+		c = Math.pow((1-5)*(2-7)/2, 2);	
+		
+		resultado = Math.pow((b - c)/10, 3);
+		
+		System.out.println(resultado);		
+		
+	}
+}
