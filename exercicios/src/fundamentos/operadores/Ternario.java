c4158f8e4980fc68b6d6244b4b90d45c83dece4f
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2019-10-28 16:12:29 -0300

    Ternarios

---
+++ exercicios/src/fundamentos/operadores/Ternario.java
@@ -0,0 +1,22 @@
+package fundamentos.operadores;
+
+public class Ternario {
+	public static void main(String[] args) {
+		double media = 5.6;
+		
+//		String resuldoParcial = media >= 5.0 ? "em recuperação" : "reprovado";
+		String resultadoFinal = media >= 7.0 ? "aprovado." : "em recuperação.";
+		
+		System.out.println("O aluno está " + resultadoFinal);
+		
+		double nota = 9.9;
+		boolean bomComportamento = true;
+		
+		boolean passouPorMedia =  nota >=7;
+		boolean temDesconto = bomComportamento && passouPorMedia;
+		
+		String resultado = temDesconto ? "Sim." : "Não.";
+		
+		System.out.println("Tem desconto? " + resultado);
+	}
+}
---
+++ exercicios/src/fundamentos/operadores/Unarios.java
@@ -0,0 +1,19 @@
+package fundamentos.operadores;
+
+public class Unarios {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		
+		a++; //a = a + 1
+		a--; //a = a - 1
+		
+		/*
+		 * Esse sempre vai ter prioridade, ou seja vai ser efetuado primeiro a operação para depois comparar.
+		 */
+		++b; //b = b + 
+		 
+		--b; //b = b - 1 
+		
+	}
+}
