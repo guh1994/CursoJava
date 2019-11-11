c60841e335ed25feb047af542dd2cf8914f88e49
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2019-11-11 17:08:14 -0300

    Desafio for

---
+++ exercicios/src/controle/DesafioFor.java
@@ -0,0 +1,25 @@
+package controle;
+
+public class DesafioFor {
+	public static void main(String[] args) {
+
+		String valor = "#";
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(valor);
+
+			valor += "#";
+		}
+
+		// Versao do desafio
+		// Não pode usar valor numerico pra controlar o laço!
+
+		//Assim
+		for (; !valor.equals("######"); valor += "#") {
+			System.out.println(valor);
+		}
+		//ou Assim
+		for(String v = "#";!v.equals("######");v+="#") {
+			System.out.println(v);
+		}
+	}
+}
--- exercicios/src/controle/For3.java
+++ exercicios/src/controle/For3.java
@@ -1,9 +1,14 @@
 package controle;
 
 public class For3 {
+	
 	public static void main(String[] args) {
+		
 		for (int i = 0; i < 10; i++) {
-			System.out.println(i);
+			for(int j = 0; j < 10; j++) {
+				System.out.printf("[%d %d]",i,j);
+			}
+			System.out.println();
 		}
 	}
 }
