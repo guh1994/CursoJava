8ea325149ef3b76dd44de447929b44fbb0d8ea45
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2020-07-07 15:22:30 -0300

    Construtor parte 1

--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -9,7 +9,7 @@ public class DataTeste {
 		data.mes = 03;
 		data.ano = 1994;
 						
-		System.out.printf("%d/%d/%d\n", data.dia, data.mes, data.ano);
+		System.out.println(data.obterDataFormatada());
 		
 		var data2 = new Data();
 		
@@ -17,6 +17,6 @@ public class DataTeste {
 		data2.mes = 07;
 		data2.ano = 2020;
 		
-		System.out.printf("%d/%d/%d\n", data2.dia, data2.mes, data2.ano);
+		System.out.println(data2.obterDataFormatada());
 	}
 }
