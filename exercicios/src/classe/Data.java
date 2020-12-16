a5e485476838e0ee0ffc880c403f66927f19809c
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2020-12-16 17:06:37 -0300

    Curso

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -11,13 +11,21 @@ public class Data {
 		mes = 1;
 		ano = 1970;
 	}
-	Data(int diaInicial,int mesInicial,int anoInicial){
-		dia = diaInicial;
-		mes = mesInicial;
-		ano = anoInicial;
+	Data(int dia,int mes,int ano){
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
 	}
 	
 	String obterDataFormatada(){
-		return String.format("%d/%d/%d ", dia,mes,ano);
+		return String.format("%d/%d/%d ", this.dia,this.mes,this.ano);
+	}
+	void imprimirDataFormatada() {
+		System.out.println(obterDataFormatada());
 	}
 }
+//byte, short, int, long ->0
+//float, double -> 0.0
+//boolean -> false
+//char -> '\u0000'
+//objetos -> null
