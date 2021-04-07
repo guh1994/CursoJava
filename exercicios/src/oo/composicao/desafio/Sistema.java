1c89da58137b772835b0a6a1197198e6088e9d0c
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2021-04-07 08:03:29 -0300

    CursoJava

---
+++ exercicios/src/oo/composicao/desafio/Sistema.java
@@ -0,0 +1,21 @@
+package oo.composicao.desafio;
+
+public class Sistema {
+
+	public static void main(String[] args) {
+		Cliente cliente = new Cliente("Maria Julia Moraes");
+		
+		Compra c1 = new Compra();
+		c1.adicionarItem("Caneta",9.67 ,100);
+		c1.adicionarItem(new Produto("Notebook",1897.88), 2);
+		
+		Compra c2 = new Compra();
+		c2.adicionarItem("Caderno",10 ,10);
+		c2.adicionarItem(new Produto("Impressora",998.90), 1);
+		
+		cliente.adicionarCompra(c1);
+		cliente.adicionarCompra(c2);
+		
+		System.out.println(cliente.obterValorTotal());
+	}
+}
